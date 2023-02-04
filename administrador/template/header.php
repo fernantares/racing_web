75fb72ee9efcf5c8a7d0e64ab61e1076dfd92508
Antares <wochordz@gmail.com>
2023-02-03 22:19:07 -0600

    ok

--- administrador/template/header.php
+++ administrador/template/header.php
@@ -7,9 +7,7 @@
     <!--<meta http-equiv="X-UA-Compatible" content="IE=edge">-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../Style.css">
-    <title><?php
-    $valorCookie = $_COOKIE['titlepage']; 
-    echo $varTitle;
+    <title><?php echo $varTitle;
     ?></title>
 </head>
 <body>
