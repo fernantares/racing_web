4a53caba0da13c4516c77779f20712752069ed8b
Antares <wochordz@gmail.com>
2023-02-04 00:59:24 -0600

    ok

---
+++ home.php
@@ -0,0 +1,8 @@
+<?php
+$host = $_SERVER['HTTP_HOST'];
+$ruta = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$html = 'vista-publico/';
+$url = "http://$host$ruta/$html";
+header("Location: $url");
+die();
+?>
