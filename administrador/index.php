ee9d8890cb5e564bccf282e67629a2de377934e8
Antares <wochordz@gmail.com>
2023-02-03 18:56:12 -0600

    ok

--- administrador/index.php
+++ administrador/index.php
@@ -4,16 +4,22 @@
     include("template/header.php"); 
 ?>
 
-        <div class="prueba">
-            <form action="controler.php" method="post">
-                <label for="make">Marca/Fabricante</label>  
-                <input id="make" type="select" name="make">
-                E-mail: 
-                <input type="text" name="email">
-                Website: 
-                <input type="text" name="website">
-                Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-            </form>   
-        </div>
+        <div class="main-dashboard">
+            <div class="dashboard-card">
+                <img src="../img/agregar_auto.png" alt="AGREGAR AUTO">
+            </div>
+            <div class="dashboard-card">
+                <img src="../img/agregar_auto.png" alt="AGREGAR AUTO">
+            </div>
+            <div class="dashboard-card">
+                <img src="../img/agregar_auto.png" alt="AGREGAR AUTO">
+            </div>
+            <div class="dashboard-card">
+                <img src="../img/agregar_auto.png" alt="AGREGAR AUTO">
+            </div>
+            <div class="dashboard-card">
+                <img src="../img/agregar_auto.png" alt="AGREGAR AUTO">
+            </div>
+        </div>    
 
 <?php include("template/footer.php"); ?>
