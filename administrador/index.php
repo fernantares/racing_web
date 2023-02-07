6133eb25e835866b70a259d51d65e76a851d9ef3
Antares <wochordz@gmail.com>
2023-02-07 08:58:24 -0600

    OK

--- administrador/index.php
+++ administrador/index.php
@@ -15,15 +15,15 @@
         <div class="dashboard">
             <?php //-----------------FILA--------------------------- ?>
             <div class="section">
-                <div id="add_car" class="dashboard-card">
+                <div id='admin_car' class="dashboard-card">
                     <img src="../img/agregar_auto.png" alt="AGREGAR AUTO">
                     <div class="descr">ADMINISTRAR AUTOS</div>
                 </div>
-                <div id="add_make" class="dashboard-card">
+                <div id="admin_make" class="dashboard-card">
                     <img src="../img/agregar_fabricante.png" alt="AGREGAR AUTO">
                     <div class="descr">ADMINISTRAR FABRICANTES</div>
                 </div>
-                <div id="add_model" class="dashboard-card">
+                <div id="admin_model" class="dashboard-card">
                     <img src="../img/agregar_modelo.png" alt="AGREGAR AUTO">
                     <div class="descr">ADMINISTRAR MODELOS</div>
                 </div>        
---
+++ mecanismo.js
@@ -0,0 +1,8 @@
+
+
+var carta = document.getElementById("admin_car");
+carta.addEventListener('click', 
+function() {
+window.location.href = 'http://localhost/racing_web/administrador/car_admin.php';
+});
+
